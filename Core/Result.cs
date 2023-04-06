2222a3cf5eeea591f875820b704e0a948532c41b
JonaVS <jon4963@gmail.com>
2023-04-06 13:59:31 -0600

    feat: add Result class
    -Class used to pass operation results

---
+++ Core/Result.cs
@@ -0,0 +1,18 @@
+namespace JobOdysseyApi.Core;
+public class Result<T>
+{
+    public bool Succeeded { get; set; }
+    public string? Error { get; set; }
+    public int ErrorCode { get; set; }
+    public T? Data { get; set; }
+
+    public static Result<T> Success(T data)
+    {
+        return new Result<T> { Succeeded = true, Data = data };
+    }
+
+    public static Result<T> Failure(string error, int errorCode)
+    {
+        return new Result<T> { Succeeded = false, Error = error, ErrorCode = errorCode };
+    }
+}
