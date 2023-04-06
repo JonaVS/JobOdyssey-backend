fc1b3216ab2b56418de15153857307dbcffae0bb
JonaVS <jon4963@gmail.com>
2023-04-05 21:10:53 -0600

    feat: add UserDto
    -Used for responses

---
+++ Dtos/UserDto.cs
@@ -0,0 +1,8 @@
+namespace JobOdysseyApi.Dtos;
+
+public class UserDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+}
