9df31f23eca360942f6d431be3928b9cd829a540
JonaVS <jon4963@gmail.com>
2023-04-07 09:55:46 -0600

    feat: add BaseApiController
    -Controller class that can be inherited by other controllers
    -The class contains a special method to handle Result types

---
+++ Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using JobOdysseyApi.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobOdysseyApi.Controllers;
+
+public class BaseApiController : ControllerBase
+{
+    protected ActionResult<T> HandleResult<T>(Result<T> result)
+    {
+        return result.Succeeded ?
+            (result.Data is not null ? Ok(result.Data) : NoContent()) : StatusCode(result.ErrorCode, result);
+    }
+}
