545f96c46c4b6675aa52e87775c582f6ec7b210f
JonaVS <jon4963@gmail.com>
2023-04-09 11:44:46 -0600

    refactor: clean up Program.cs file
    -AddJwtAuthentication service extention method is now used
    -Minor code style changes applied

--- Program.cs
+++ Program.cs
@@ -1,46 +1,22 @@
-using System.Text;
 using JobOdysseyApi.Configurations;
 using JobOdysseyApi.Data;
 using JobOdysseyApi.Models;
 using JobOdysseyApi.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 DotNetEnv.Env.Load();
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSqlite<AppDbContext>(DotNetEnv.Env.GetString("DB_CONNECTION"));
 builder.Services.AddAutoMapper(typeof(MapperConfig));
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(jwt =>
-{
-    var key = Encoding.ASCII.GetBytes(DotNetEnv.Env.GetString("JWT_SECRET"));
-    jwt.SaveToken = true;
-    jwt.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-    };
-});
-
+builder.Services.AddJwtAuthentication();
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedEmail = false)
     .AddEntityFrameworkStores<AppDbContext>()
     .AddSignInManager<SignInManager<ApplicationUser>>();
-
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<AuthService>();
 
@@ -54,11 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
