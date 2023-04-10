dce52b53dbf3e8a3bade9db6b0c8a3a1efb5cd85
JonaVS <jon4963@gmail.com>
2023-04-10 15:45:26 -0600

    feat: add RefreshToken logic at AuthTokensService
    -Private method for RefreshToken generation
    -Private helper method for RefreshToken random string generation
    -Public method that generates both access token and refresh token

--- Services/AuthTokensService.cs
+++ Services/AuthTokensService.cs
@@ -1,14 +1,43 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
-using JobOdysseyApi.Models;
 using Microsoft.IdentityModel.Tokens;
+using JobOdysseyApi.Core;
+using JobOdysseyApi.Data;
+using JobOdysseyApi.Dtos;
+using JobOdysseyApi.Models;
 
 namespace JobOdysseyApi.Services;
 
 public class AuthTokensService
 {
     private readonly string JwtSecret = DotNetEnv.Env.GetString("JWT_SECRET");
+    private SecurityToken? generatedToken;
+    private readonly AppDbContext _dbContext;
+    public AuthTokensService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Result<AuthTokensDto>> GenerateAuthTokens(ApplicationUser user) {
+        try
+        {
+            var authTokens = new AuthTokensDto()
+            {
+                Token = GenerateJwtToken(user),
+                RefreshToken = await GenerateRefreshToken(user.Id)
+            };
+
+            return Result<AuthTokensDto>.Success(authTokens);
+        }
+        catch (Exception)
+        { 
+           return Result<AuthTokensDto>.Failure("An error has occurred.", (int)HttpStatusCode.InternalServerError); 
+        }
+    }
+
     public string GenerateJwtToken(ApplicationUser user) 
     {
         var jwtTokenHandler = new JwtSecurityTokenHandler();
@@ -29,7 +58,38 @@ public class AuthTokensService
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
         };
 
-        var token = jwtTokenHandler.CreateToken(tokenDescriptor);
-        return jwtTokenHandler.WriteToken(token);
+        generatedToken = jwtTokenHandler.CreateToken(tokenDescriptor);
+
+        return jwtTokenHandler.WriteToken(generatedToken);
+    }
+
+    private async Task<string> GenerateRefreshToken(string userId)
+    {
+
+        var refreshToken = new RefreshToken()
+        {
+            JwtId = generatedToken!.Id,
+            UserId = userId,
+            Token = GenerateRefreshTokenString(),
+            IssuedAt = DateTime.UtcNow,
+            ExpiresAt = DateTime.UtcNow.AddMonths(1),
+            IsRevoked = false,
+            IsUsed = false
+        };
+
+        await _dbContext.RefreshTokens!.AddAsync(refreshToken);
+        await _dbContext.SaveChangesAsync();
+
+        return refreshToken.Token;
+    }
+
+    private string GenerateRefreshTokenString()
+    {
+        var randomNumber = new byte[32];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
     }
 }
