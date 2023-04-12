8f6e81f864a28e954e0095991f14a6f63f2e7eda
JonaVS <jon4963@gmail.com>
2023-04-12 00:06:10 -0600

    feat: allow HandleResult method override

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -5,7 +5,7 @@ namespace JobOdysseyApi.Controllers;
 
 public class BaseApiController : ControllerBase
 {
-    protected ActionResult<T> HandleResult<T>(Result<T> result)
+    protected virtual ActionResult<T> HandleResult<T>(Result<T> result)
     {
         return result.Succeeded ?
             (result.Data is not null ? Ok(result.Data) : NoContent()) : StatusCode(result.ErrorCode, result);
