80535033ef5bd958afe2490104db923434974090
JonaVS <jon4963@gmail.com>
2023-04-12 00:16:44 -0600

    refactor: remove RefreshToken field from JSON response
    -The field is just used internally by the server

--- Dtos/AuthResponseDto.cs
+++ Dtos/AuthResponseDto.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace JobOdysseyApi.Dtos;
 
 public class AuthResponseDto
@@ -5,5 +7,7 @@ public class AuthResponseDto
     public string DisplayName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Token { get; set; } = string.Empty;
+    
+    [JsonIgnore]
     public string RefreshToken { get; set; } = string.Empty;
 }
