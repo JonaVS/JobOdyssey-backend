f386804389d1d6950de775f68b5468b7e30ec804
JonaVS <jon4963@gmail.com>
2023-04-13 15:11:33 -0600

    feat: add User field to AuthTokensDto
    -Field needed during refresh token API call

--- Dtos/AuthTokensDto.cs
+++ Dtos/AuthTokensDto.cs
@@ -1,7 +1,10 @@
+using JobOdysseyApi.Models;
+
 namespace JobOdysseyApi.Dtos;
 
 public class AuthTokensDto
 {
     public string Token { get; set; } = string.Empty;
     public string RefreshToken { get; set; } = string.Empty;
+    public ApplicationUser? User { get; set; }
 }
