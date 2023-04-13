c1fc7ae9c0d78de1c6815912e34cc78d70cff229
JonaVS <jon4963@gmail.com>
2023-04-13 16:53:56 -0600

    refactor: add default values to LoginRequestDto fields

--- Dtos/LoginRequestDto.cs
+++ Dtos/LoginRequestDto.cs
@@ -6,8 +6,8 @@ public class LoginRequestDto
 {    
     [Required]
     [EmailAddress(ErrorMessage = "The Email field is not a valid")]
-    public string? Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     [Required]
-    public string? Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 }
