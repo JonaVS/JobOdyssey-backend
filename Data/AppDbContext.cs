99083020bc198da3c8db06fb9fdf6b4e0fa65366
JonaVS <jon4963@gmail.com>
2023-04-13 15:38:51 -0600

    refactor: use null-forgiving operator at AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,7 +6,7 @@ namespace JobOdysseyApi.Data;
 
 public class AppDbContext : IdentityDbContext<ApplicationUser> 
 {
-    public DbSet<RefreshToken>? RefreshTokens { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 }
