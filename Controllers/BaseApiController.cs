53dc3db53d0054d1dafbd920080813746b1f93a5
JonaVS <jon4963@gmail.com>
2023-04-14 00:14:18 -0600

    refactor: use ErrorResponse at BaseApiController
    -Used at HandleResult method

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -8,6 +8,7 @@ public class BaseApiController : ControllerBase
     protected virtual ActionResult<T> HandleResult<T>(Result<T> result)
     {
         return result.Succeeded ?
-            (result.Data is not null ? Ok(result.Data) : NoContent()) : StatusCode(result.ErrorCode, result);
+            (result.Data is not null ? Ok(result.Data) : NoContent()) 
+            : StatusCode(result.ErrorCode, ErrorResponse.Generate("Action error", result.ErrorCode, result.Error!));
     }
 }
