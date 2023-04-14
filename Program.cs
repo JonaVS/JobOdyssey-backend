b2301e7c5c55761f2fb8d70213dd16611066cd6e
JonaVS <jon4963@gmail.com>
2023-04-14 00:26:41 -0600

    feat: enable validation filters
    -This disables the default model validation filter
    -Injects the custom filters to the DI container

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using JobOdysseyApi.Configurations;
 using JobOdysseyApi.Data;
+using JobOdysseyApi.Filters;
 using JobOdysseyApi.Models;
 using JobOdysseyApi.Services;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,7 @@ DotNetEnv.Env.Load();
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
 builder.Services.AddSwaggerGen();
 builder.Services.AddSqlite<AppDbContext>(DotNetEnv.Env.GetString("DB_CONNECTION"));
 builder.Services.AddAutoMapper(typeof(MapperConfig));
@@ -19,6 +22,8 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddSignInManager<SignInManager<ApplicationUser>>();
 builder.Services.AddScoped<AuthTokensService>();
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<ValidationFilterAttribute>();
+builder.Services.AddScoped<RefreshTokenValidationFilterAttribute>();
 
 var app = builder.Build();
 
