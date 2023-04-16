d1569e3eb5b9a9037a6f661934de608e0cccde0e
JonaVS <jon4963@gmail.com>
2023-04-16 00:57:14 -0600

    refactor: set default value for Error field in Result class

--- Core/Result.cs
+++ Core/Result.cs
@@ -2,7 +2,7 @@ namespace JobOdysseyApi.Core;
 public class Result<T>
 {
     public bool Succeeded { get; set; }
-    public string? Error { get; set; }
+    public string Error { get; set; } = string.Empty;
     public int ErrorCode { get; set; }
     public T? Data { get; set; }
 
