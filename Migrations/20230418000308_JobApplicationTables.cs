35a7bf34fa3710e9b434eb6f8f48d0f8af934126
JonaVS <jon4963@gmail.com>
2023-04-17 18:24:38 -0600

    feat: add db migration for Job application tables
    -Table for Job application boards
    -Table for Job applications

---
+++ Migrations/20230418000308_JobApplicationTables.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobOdysseyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobApplicationTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:job_application_status", "applied,interviewing,offer,rejected,ghosted,withdrawn");
+
+            migrationBuilder.CreateTable(
+                name: "JobApplicationBoards",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplicationBoards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobApplicationBoards_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobApplications",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    JobTitle = table.Column<string>(type: "text", nullable: false),
+                    CompanyName = table.Column<string>(type: "text", nullable: false),
+                    ApplicationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    JobDescription = table.Column<string>(type: "text", nullable: true),
+                    JobUrl = table.Column<string>(type: "text", nullable: true),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    JobBoardId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplications", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobApplications_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JobApplications_JobApplicationBoards_JobBoardId",
+                        column: x => x.JobBoardId,
+                        principalTable: "JobApplicationBoards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplicationBoards_UserId",
+                table: "JobApplicationBoards",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_JobBoardId",
+                table: "JobApplications",
+                column: "JobBoardId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplications_UserId",
+                table: "JobApplications",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobApplications");
+
+            migrationBuilder.DropTable(
+                name: "JobApplicationBoards");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:Enum:job_application_status", "applied,interviewing,offer,rejected,ghosted,withdrawn");
+        }
+    }
+}
