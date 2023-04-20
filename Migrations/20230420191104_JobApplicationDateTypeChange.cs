49dd288b295e53a75048430983f1e033be025546
JonaVS <jon4963@gmail.com>
2023-04-20 13:31:21 -0600

    feat: add migration for JobApplication field change

---
+++ Migrations/20230420191104_JobApplicationDateTypeChange.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobOdysseyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobApplicationDateTypeChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
