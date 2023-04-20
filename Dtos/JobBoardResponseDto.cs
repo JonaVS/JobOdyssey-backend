b64da5b1afb191fb372601af758e10454baa2d46
JonaVS <jon4963@gmail.com>
2023-04-19 18:34:48 -0600

    feat: add CreatedAt to JobBoardResponseDto

--- Dtos/JobBoardResponseDto.cs
+++ Dtos/JobBoardResponseDto.cs
@@ -4,4 +4,5 @@ public class JobBoardResponseDto
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
 } 
