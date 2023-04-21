309253c04e1a206d32c0b024a41672d63606a2b6
JonaVS <jon4963@gmail.com>
2023-04-21 15:03:12 -0600

    feat: add MaxLength validation for Name in CreateJobBoardDto

--- Dtos/JobApplicationBoard/CreateJobBoardDto.cs
+++ Dtos/JobApplicationBoard/CreateJobBoardDto.cs
@@ -5,5 +5,6 @@ namespace JobOdysseyApi.Dtos;
 public class CreateJobBoardDto
 {
     [Required]
+    [MaxLength(50, ErrorMessage = "The Name field must have a maximum length of 50 characters.")]
     public string Name { get; set; } = string.Empty;
 }
