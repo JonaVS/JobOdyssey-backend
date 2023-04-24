eeea39d0bae937035c0e3bad09e36c38ef752914
JonaVS <jon4963@gmail.com>
2023-04-23 19:09:07 -0600

    feat: enable create job application endpoint

---
+++ Controllers/JobApplicationController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using JobOdysseyApi.Dtos;
+using JobOdysseyApi.Filters;
+using JobOdysseyApi.Services;
+
+namespace JobOdysseyApi.Controllers;
+
+[ApiController]
+[Route("api/job-applications")]
+[Authorize]
+public class JobApplicationController : BaseApiController
+{
+    private readonly JobApplicationService _jobApplicationService;
+
+    public JobApplicationController(JobApplicationService jobApplicationService)
+    {
+        _jobApplicationService = jobApplicationService;
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<ActionResult<JobApplicationDto>> CreateJobApplication(CreateJobApplicationDto requestDto)
+    {
+        return HandleResult<JobApplicationDto>(await _jobApplicationService.CreateJobApplication(requestDto));
+    }
+}
