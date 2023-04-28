14c5df4210e092afd3c6b4631cc6a7fd2f440c6f
JonaVS <jon4963@gmail.com>
2023-04-28 16:43:03 -0600

    feat: add max length to JobApplication model fields
    -For: JobTitle, CompanyName, JobDescription and Notes

--- Models/JobApplication.cs
+++ Models/JobApplication.cs
@@ -16,13 +16,17 @@ public class JobApplication : TimeTrackableEntity
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     [Required]
+    [MaxLength(50)]
     public string JobTitle { get; set; } = string.Empty;
     [Required]
+    [MaxLength(50)]
     public string CompanyName { get; set; } = string.Empty;
     [Required]
     public DateTimeOffset ApplicationDate { get; set; }
+    [MaxLength(2000)]
     public string? JobDescription { get; set; }
     public string? JobUrl { get; set; }
+    [MaxLength(2000)]
     public string? Notes { get; set; }
     [Required]
     public JobApplicationStatus Status { get; set; }
