52e23dbe7ead145f9e939e0f8ead47eea0ffae28
JonaVS <jon4963@gmail.com>
2023-05-09 16:09:11 -0600

    feat: add AuthenticatedUserDto

---
+++ Dtos/Auth/AuthenticatedUser.cs
@@ -0,0 +1,7 @@
+namespace JobOdysseyApi.Dtos;
+
+public class AuthenticatedUserDto
+{
+    public string DisplayName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
